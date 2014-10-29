d1a87f1ebdb541d38c877bd9aba7239f8fd12e0e
lancy <lancy@fenbi.com>
2014-10-29 12:40:35 +0800

    ADD: copyright header

--- YTKNetwork/YTKNetworkPrivate.h
+++ YTKNetwork/YTKNetworkPrivate.h
@@ -1,18 +1,29 @@
 //
-//  FenbiBaseRequestUtils.h
-//  Solar
+//  YTKNetworkPrivate.h
 //
-//  Created by tangqiao on 8/6/14.
-//  Copyright (c) 2014 fenbi. All rights reserved.
+//  Copyright (c) 2012-2014 YTKNetwork https://github.com/yuantiku
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
-#ifdef DEBUG
-#define YTKLog(...)    NSLog(__VA_ARGS__)
-#else
-#define YTKLog(...)
-#endif
+FOUNDATION_EXPORT void YTKLog(NSString *format, ...) NS_FORMAT_FUNCTION(1,2);
 
 @interface YTKNetworkPrivate : NSObject
 
@@ -28,4 +39,5 @@
 + (NSString *)appVersionString;
 
 @end
+
 
