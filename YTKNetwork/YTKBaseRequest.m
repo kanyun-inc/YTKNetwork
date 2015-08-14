17bc83cca07e5e15758327412823ef637336887f
钟宝健 <aboojaner@gmail.com>
2015-08-14 17:16:44 +0800

    通过运行时来动态获取请求参数

--- YTKNetwork/YTKBaseRequest.m
+++ YTKNetwork/YTKBaseRequest.m
@@ -24,6 +24,7 @@
 #import "YTKBaseRequest.h"
 #import "YTKNetworkAgent.h"
 #import "YTKNetworkPrivate.h"
+#import <objc/runtime.h>
 
 @implementation YTKBaseRequest
 
@@ -51,7 +52,29 @@
 }
 
 - (id)requestArgument {
-    return nil;
+    unsigned int propCount;
+    objc_property_t* properties = class_copyPropertyList([self class], &propCount);
+    
+    NSMutableDictionary *parmas = [[NSMutableDictionary alloc] initWithCapacity:propCount];
+    
+    for (int i=0; i<propCount; i++) {
+        
+        objc_property_t prop = properties[i];
+        const char *propName = property_getName(prop);
+        
+        if(propName) {
+            
+            NSString *key = [NSString stringWithCString:propName encoding:NSUTF8StringEncoding];
+            
+            id value = objc_msgSend(self, sel_registerName(propName));
+            
+            [parmas setObject:value forKey:key];
+            
+        }
+        
+    }
+    
+    return parmas;
 }
 
 - (id)cacheFileNameFilterForRequestArgument:(id)argument {
