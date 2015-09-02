5e4c5949c6bfa5a5bafc4c9ff1c8dd255b53389a
Wei Han <xingheng907@hotmail.com>
2015-09-02 15:44:27 +0800

    Fixed the nil value validation.

--- YTKNetwork/YTKNetworkPrivate.m
+++ YTKNetwork/YTKNetworkPrivate.m
@@ -46,7 +46,10 @@ void YTKLog(NSString *format, ...) {
         while ((key = [enumerator nextObject]) != nil) {
             id value = dict[key];
             id format = validator[key];
-            if ([value isKindOfClass:[NSDictionary class]]
+            
+            if (!value) {
+                break;
+            } else if ([value isKindOfClass:[NSDictionary class]]
                 || [value isKindOfClass:[NSArray class]]) {
                 result = [self checkJson:value withValidator:format];
                 if (!result) {
