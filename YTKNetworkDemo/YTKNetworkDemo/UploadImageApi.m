059f0ac2a378c99597907c19b8dc9abc825b86ec
汪顺舟 <wangsz@corp.hxqc.com>
2015-10-23 11:27:02 +0800

    ADD: 完善上传图片的DEMO。

--- YTKNetworkDemo/YTKNetworkDemo/UploadImageApi.m
+++ YTKNetworkDemo/YTKNetworkDemo/UploadImageApi.m
@@ -24,17 +24,27 @@
     return YTKRequestMethodPost;
 }
 
+//- (id)requestArgument {
+//    return @{ @"token": @"123123" };
+//}
+
 - (NSString *)requestUrl {
-    return @"/iphone/image/upload";
+    return @"http://10.0.12.221:8080/Api/Users/avatar";
 }
 
 - (AFConstructingBlock)constructingBodyBlock {
     return ^(id<AFMultipartFormData> formData) {
         NSData *data = UIImageJPEGRepresentation(_image, 0.9);
-        NSString *name = @"image";
-        NSString *formKey = @"image";
+        NSString *name = @"nameOfFile";
+        NSString *formKey = @"avatar";
         NSString *type = @"image/jpeg";
         [formData appendPartWithFileData:data name:formKey fileName:name mimeType:type];
+    };
+}
+
+- (AFUploadProgressBlock)uploadProgressBlock{
+    return ^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite){
+        NSLog(@"==============\nbytesWritten: %ld \ntotalBytesWritten: %lld \ntotalBytesExpectedToWrite: %lld",bytesWritten,totalBytesWritten, totalBytesExpectedToWrite);
     };
 }
 
