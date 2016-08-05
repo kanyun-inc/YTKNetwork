4616e777e54d795bdef2f8cfa491b72c404ebf00
Sxd <shixiaoda@qq.com>
2016-08-05 11:30:24 +0800

    fix demo

--- YTKNetworkDemo/YTKNetworkDemo/ViewController.m
+++ YTKNetworkDemo/YTKNetworkDemo/ViewController.m
@@ -36,23 +36,27 @@
     return [hash lowercaseString];
 }
 
+- (NSString *)cachePath
+{
+    NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *cachePath = [libPath stringByAppendingPathComponent:@"Caches"];
+    return cachePath;
+}
+
 /// Send batch request
 - (void)sendBatchRequest {
     GetImageApi *a = [[GetImageApi alloc] initWithImageId:@"site-503(new)/1/113523/coverbig.jpg"];
     GetImageApi *b = [[GetImageApi alloc] initWithImageId:@"site-532(new)/0/10/coverbig.jpg"];
     GetImageApi *c = [[GetImageApi alloc] initWithImageId:@"site-519(new)/3/307145/coverbig.jpg"];
-//    GetUserInfoApi *d = [[GetUserInfoApi alloc] initWithUserId:@"123"];
     YTKBatchRequest *batchRequest = [[YTKBatchRequest alloc] initWithRequestArray:@[a, b, c]];
     [batchRequest startWithCompletionBlockWithSuccess:^(YTKBatchRequest *batchRequest) {
         NSLog(@"succeed");
-        NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-        NSString *cachePath = [libPath stringByAppendingPathComponent:@"Caches"];
         
         NSArray *requests = batchRequest.requestArray;
         for (NSInteger i = 0; i < 3; i++)
         {
             GetImageApi *imgAPI = (GetImageApi *)requests[i];
-            NSString *filePath = [cachePath stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
+            NSString *filePath = [[self cachePath] stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
             UIImage *image = [UIImage imageWithContentsOfFile:filePath];
             UIImageView *imageView = [self.view viewWithTag:1000+i];
             imageView.image = image;
@@ -63,15 +67,12 @@
 }
 
 - (void)sendChainRequest {
-//    RegisterApi *reg = [[RegisterApi alloc] initWithUsername:@"username" password:@"password"];
     GetImageApi *a = [[GetImageApi alloc] initWithImageId:@"site-503(new)/1/113523/coverbig.jpg"];
     YTKChainRequest *chainReq = [[YTKChainRequest alloc] init];
     [chainReq addRequest:a callback:^(YTKChainRequest *chainRequest, YTKBaseRequest *baseRequest) {
         
-        NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-        NSString *cachePath = [libPath stringByAppendingPathComponent:@"Caches"];
         GetImageApi *imgAPI = (GetImageApi *)baseRequest;
-        NSString *filePath = [cachePath stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
+        NSString *filePath = [[self cachePath] stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
         UIImage *image = [UIImage imageWithContentsOfFile:filePath];
         UIImageView *imageView = [self.view viewWithTag:1000+3];
         imageView.image = image;
@@ -79,21 +80,16 @@
         GetImageApi *b = [[GetImageApi alloc] initWithImageId:@"site-532(new)/0/10/coverbig.jpg"];
         [chainRequest addRequest:b callback:^(YTKChainRequest * _Nonnull chainRequest, YTKBaseRequest * _Nonnull baseRequest) {
             
-            NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-            NSString *cachePath = [libPath stringByAppendingPathComponent:@"Caches"];
             GetImageApi *imgAPI = (GetImageApi *)baseRequest;
-            NSString *filePath = [cachePath stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
+            NSString *filePath = [[self cachePath] stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
             UIImage *image = [UIImage imageWithContentsOfFile:filePath];
             UIImageView *imageView = [self.view viewWithTag:1000+4];
             imageView.image = image;
             
             GetImageApi *c = [[GetImageApi alloc] initWithImageId:@"site-519(new)/3/307145/coverbig.jpg"];
             [chainRequest addRequest:c callback:^(YTKChainRequest * _Nonnull chainRequest, YTKBaseRequest * _Nonnull baseRequest) {
-                
-                NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-                NSString *cachePath = [libPath stringByAppendingPathComponent:@"Caches"];
                 GetImageApi *imgAPI = (GetImageApi *)baseRequest;
-                NSString *filePath = [cachePath stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
+                NSString *filePath = [[self cachePath] stringByAppendingPathComponent:[self MD5:imgAPI.imageId]];
                 UIImage *image = [UIImage imageWithContentsOfFile:filePath];
                 UIImageView *imageView = [self.view viewWithTag:1000+5];
                 imageView.image = image;
@@ -136,9 +132,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     for (NSInteger i = 0; i < 3; i ++)
     {
-        UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(5/2+125*i, 64, 120, 160)];
+        UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(5/2+125*i, 20, 120, 160)];
         imgView.backgroundColor = [UIColor grayColor];
         [self.view addSubview:imgView];
         imgView.tag = 1000+i;
@@ -149,12 +146,12 @@
     [btn setTitle:@"sendBatchRequest" forState:UIControlStateNormal];
     [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [btn setBackgroundColor:[UIColor redColor]];
-    btn.frame = CGRectMake(10, 64+160+20, 150, 20);
+    btn.frame = CGRectMake(10, 20+160+20, 150, 20);
     [btn addTarget:self action:@selector(sendBatchRequest) forControlEvents:UIControlEventTouchUpInside];
     
     for (NSInteger i = 0; i < 3; i ++)
     {
-        UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(5/2+125*i, 64+160+20+20+20, 120, 160)];
+        UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(5/2+125*i, 20+160+20+20+20, 120, 160)];
         imgView.backgroundColor = [UIColor grayColor];
         [self.view addSubview:imgView];
         imgView.tag = 1000+i+3;
@@ -165,7 +162,7 @@
     [btn2 setTitle:@"sendChainRequest" forState:UIControlStateNormal];
     [btn2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [btn2 setBackgroundColor:[UIColor redColor]];
-    btn2.frame = CGRectMake(10, 64+160+20+20+160+20+20, 150, 20);
+    btn2.frame = CGRectMake(10, 20+160+20+20+160+20+20, 150, 20);
     [btn2 addTarget:self action:@selector(sendChainRequest) forControlEvents:UIControlEventTouchUpInside];
 
 }
